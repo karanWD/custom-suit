ccbf46dec4a36b3c1c3f8fb6acf73e9c256d4600
Karan Shams <shams@Parmo.com>
2021-10-12 11:25:23 +0330

    homepage updated

---
+++ src/Components/priceContainer/PriceContainer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const PriceContainer = () =>{
+    return(
+        <div className="price">
+            <span className="d-block price-toman text-right">تومان</span>
+            <h6 className="d-block text-right">
+                15,000,000
+            </h6>
+        </div>
+    )
+}
+
+export default PriceContainer
